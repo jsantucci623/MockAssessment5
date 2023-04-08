4e91bac7d286fa83d7bf1baf819d9e505163444f
Jeff Santucci <jeffsantucci@rocketmortgage.com>
2023-04-08 18:45:39 -0400

    Mock Assessment 5 Jeff Santucci

--- MockAssessment5/MockAssessment5/Controllers/HomeController.cs
+++ MockAssessment5/MockAssessment5/Controllers/HomeController.cs
@@ -18,10 +18,41 @@ namespace MockAssessment5.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+/*        public IActionResult Result(Person)
         {
             return View();
         }
+*/        
+        [HttpPost]
+        public IActionResult Result(Person person)
+        {
+            if (!ModelState.IsValid)    // very handy tool
+            {
+                return RedirectToAction("Index");
+            }
+            // We can process the UserProfile object now!
+            // For example, we can save it to a database.
+
+            if (person.Age >= 21) {
+                person.CanDrink = true;
+            }
+            else person.CanDrink = false;
+
+            if (person.Age >= 16) { 
+                person.CanDrive = true;
+            }
+            else person.CanDrive = false;
+
+            return View("Result", person);
+        }
+
+       
+        public IActionResult GetAge(Person person)
+        {
+            
+            return View(person);
+        }
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ MockAssessment5/MockAssessment5/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace MockAssessment5.Models
+{
+    public class Person
+    {
+        public string? UserName { get; set; }
+        public int Age { get; set; }
+
+        public bool CanDrink { get; set;}
+
+        public bool CanDrive { get; set; }
+    }
+}
